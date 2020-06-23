333325536cba38eefb880015501424d924b1c998
Jesu Castillo <jcastillo@MacBook-Pro-van-Jesu.local>
2020-06-23 19:50:54 +0200

    Completed module

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf-8')
 const accounts = JSON.parse(accountData)
@@ -22,6 +23,24 @@ app.get('/savings', (_,res) => res.render('account', {account: accounts.savings}
 app.get('/checking', (_,res) => res.render('account', {account: accounts.checking}))
 app.get('/credit', (_,res) => res.render('account', {account: accounts.credit}))
 app.get('/profile', (_,res) => res.render('profile', {user: users[0]}))
+app.get('/transfer', (_,res) => res.render('transfer'))
+app.post('/transfer', ({body}, res)=>{
+    const fromAccount = accounts[body.from]
+    const toAccount = accounts[body.to]
+    toAccount.balance += parseInt(body.amount)
+    fromAccount.balance -= parseInt(body.amount)
+    const accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf-8')
+    res.render('transfer', {message: "Transfer Completed"})
+})
+app.get('/payment', (_,res) => res.render('payment', {account: accounts.credit}))
+app.post('/payment', ({body},res) => {
+    accounts.credit.balance -= parseInt(body.amount)
+    accounts.credit.available += parseInt(body.amount)
+    const accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf-8')
+    res.render('payment', {account: accounts.credit, message: 'Payment Successful'})
+})
 
 app.listen(3000, ()=>{
     console.log('PS Project Running on port 3000!')
